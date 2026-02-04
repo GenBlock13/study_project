dfc64619b1f916e08407d23368d7dc21fee0d2ec
Reptilius <blockgen@yandex.ru>
2026-02-04 20:20:58 +0300

    pureState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { TEST_1, TEST_2} from './test';
+
+import Bat from './media/bat.png';
+import { names } from './test'; 
+import './media/styles.css'
+
+console.log(TEST_1, TEST_2, names);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <p>Hello, World!</p>
+    </div>
+
+    <div className="hello">
+      <img src={Bat}/>
+    </div>
+    
+    
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
